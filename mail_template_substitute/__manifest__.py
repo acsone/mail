b08a39ac0be8a8360f5ad18d44e5b77e6d09dce7
eugenio <eugenio@studio73.es>
2025-10-16 09:35:54 +0200

    [IMP] mail_template_substitute: pre-commit auto fixes

--- mail_template_substitute/__manifest__.py
+++ mail_template_substitute/__manifest__.py
@@ -9,7 +9,7 @@
     "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/social",
+    "website": "https://github.com/OCA/mail",
     "depends": ["base", "mail"],
     "data": [
         "security/mail_template_substitution_rule.xml",
